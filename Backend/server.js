46a350f0e22566afed2bbbdd59c234844488b482
Bytesoft <bytesofthq@gmail.com>
2026-06-21 11:54:25 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -36,7 +36,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
   'https://nekkamfoundation.in',
-  'https://www.nekkamfoundation.in'
+  'https://www.nekkamfoundation.in',
   process.env.FRONTEND_URL
 ];
 
